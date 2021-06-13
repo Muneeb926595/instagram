43521fd651ac0ecbf576278d97036e7b0971531a
Muneeb926595 <muneebkhann0@gmail.com>
2021-06-13 11:36:52 +0500

    single theme

--- src/@styles/index.js
+++ src/@styles/index.js
@@ -4,3 +4,4 @@ export { default as boxStyles } from "./boxStyles";
 export { default as inputStyles } from "./inputStyles";
 export { default as buttonStyles } from "./buttonStyles";
 export { default as textStyles } from "./textStyles";
+export { default as theme } from "./theme";
